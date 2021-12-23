4f674160357d38f0f9a0aed9d86656175239e476
ysu96 <ysu96@naver.com>
2021-12-23 23:58:42 +0900

    boj-1009

---
+++ JAVA/BOJ/구현/BOJ_1009.java
@@ -0,0 +1,28 @@
+package 구현;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1009 {
+    public static int T,a,b;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        for(int tt =0 ; tt<T; tt++){
+            st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            int tmp = a;
+            for(int i=1; i<b; i++){
+                tmp*=a;
+                tmp = tmp%10;
+            }
+            if(tmp == 0) tmp = 10;
+            System.out.println(tmp);
+        }
+    }
+}
