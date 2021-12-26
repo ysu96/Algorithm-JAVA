3265fa1e1bbf3b4baff101c0dfe6cf90138bcac3
ysu96 <ysu96@naver.com>
2021-12-26 20:38:29 +0900

    boj 2565 전기줄 : DP, LIS 응용

---
+++ JAVA/BOJ/다이나믹프로그래밍/BOJ_2565.java
@@ -0,0 +1,45 @@
+package 다이나믹프로그래밍;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+// LIS 응용
+public class BOJ_2565 {
+    public static int N;
+    public static int[][] arr;
+    public static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][2];
+        dp = new int[N];
+        StringTokenizer st;
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+        int ans = 0;
+        for(int i=0; i<N; i++){
+            dp[i] = 1;
+            for(int j=0; j<i; j++){
+                if (arr[i][1] > arr[j][1]) {
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                }
+            }
+            ans = Math.max(ans, dp[i]);
+        }
+        System.out.println(N-ans);
+    }
+}
