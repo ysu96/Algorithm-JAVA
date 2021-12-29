1e851aec5cf86f137faf88c6f8e3cf4c3f19cda3
ysu96 <ysu96@naver.com>
2021-12-29 14:18:37 +0900

    boj 2193 dp

---
+++ JAVA/BOJ/다이나믹프로그래밍/BOJ_2193.java
@@ -0,0 +1,22 @@
+package 다이나믹프로그래밍;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//이친수
+public class BOJ_2193 {
+    public static int N;
+    public static long[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        dp = new long[91];
+        dp[1] = 1;
+        dp[2] = 1;
+        for(int i=3; i<=N; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        System.out.println(dp[N]);
+    }
+}
