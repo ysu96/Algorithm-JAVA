96a2fca621200dad8b16dc61304ca40b71a4b799
ysu96 <ysu96@naver.com>
2022-01-09 00:23:10 +0900

    BOJ 1629 곱셈 : 거듭제곱 알고리즘, 분할정복

---
+++ JAVA/BOJ/수학/BOJ_1629.java
@@ -0,0 +1,32 @@
+package 수학;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1629 {
+    public static int A,B,C;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        long answer = pow(A, B, C);
+        System.out.println(answer%C);
+    }
+
+    public static long pow(int a, int b, int c){
+        if(b == 0) return 1;
+        else if(b==1) return a;
+        else if(b%2 == 0){
+            long n = pow(a, b/2, c) % c;
+            return (n*n)%c;
+        }else{
+            long n = pow(a, b/2, c) % c;
+            return (((n*n)%c) * a)%c;
+        }
+    }
+}
