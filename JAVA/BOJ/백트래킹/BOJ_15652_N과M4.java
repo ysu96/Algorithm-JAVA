3e5b7a78a4eed1e7dbe8f692e7348ff5d51d2cfd
ysu96 <ysu96@naver.com>
2022-01-20 20:52:05 +0900

    boj 15652

---
+++ JAVA/BOJ/백트래킹/BOJ_15652_N과M4.java
@@ -0,0 +1,39 @@
+package JAVA.BOJ.백트래킹;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_15652_N과M4 {
+    public static int N, M;
+    public static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+        solution(1, 0);
+    }
+
+
+    public static void solution(int n, int idx) {
+        if (idx == M) {
+            //출력
+
+            for (int i : arr) {
+                System.out.print(Integer.toString(i) + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = n; i <= N; i++) {
+            arr[idx] = i;
+            solution(i, idx + 1);
+        }
+
+    }
+}
