ec42047e1225ecc84f1d55003184de160e9ed858
ysu96 <ysu96@naver.com>
2022-03-04 19:55:49 +0900

    BOJ 9252 LCS2 : DP

--- JAVA/BOJ/다이나믹프로그래밍/BOJ_9252_LCS2.java
+++ JAVA/BOJ/다이나믹프로그래밍/BOJ_9252_LCS2.java
@@ -3,7 +3,7 @@ package JAVA.BOJ.다이나믹프로그래밍;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.Stack;
 
 public class BOJ_9252_LCS2 {
     public static String s1,s2;
@@ -13,17 +13,15 @@ public class BOJ_9252_LCS2 {
         s1 = br.readLine();
         s2 = br.readLine();
 
-        String[][] dp = new String[s1.length() + 1][s2.length() + 1];
-        for(int i=0; i<=s1.length(); i++){
-            Arrays.fill(dp[i], "");
-        }
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+
         for(int i=1; i<=s1.length(); i++){
             for(int j=1; j<=s2.length(); j++){
                 if (s1.charAt(i-1) == s2.charAt(j-1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + s1.charAt(i-1);
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
                 }
                 else{
-                    if (dp[i][j - 1].length() > dp[i - 1][j].length()) {
+                    if (dp[i][j - 1] > dp[i - 1][j]) {
                         dp[i][j] = dp[i][j - 1];
                     }else{
                         dp[i][j] = dp[i-1][j];
@@ -31,7 +29,27 @@ public class BOJ_9252_LCS2 {
                 }
             }
         }
-        System.out.println(dp[s1.length()][s2.length()].length());
-        System.out.println(dp[s1.length()][s2.length()]);
+
+        Stack<Character> stack = new Stack<>();
+        int n = s1.length();
+        int m = s2.length();
+        while (n > 0 && m > 0) {
+            if (dp[n][m] == dp[n][m - 1]) {
+                m--;
+            } else if (dp[n][m] == dp[n - 1][m]) {
+                n--;
+            }else{
+                stack.add(s1.charAt(n-1));
+                n--;
+                m--;
+            }
+        }
+        String answer = "";
+        int cnt = dp[s1.length()][s2.length()];
+        while(!stack.isEmpty()){
+            answer += stack.pop();
+        }
+        System.out.println(cnt);
+        if(cnt != 0) System.out.println(answer);
     }
 }
