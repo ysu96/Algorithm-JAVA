11a42a9f3820eadf44d0eca606ed483645d2a9dc
ysu96 <ysu96@naver.com>
2022-03-09 21:22:31 +0900

    2022 카카오

--- JAVA/Programmers/src/KAKAO2022/p2_주차요금계산.java
+++ JAVA/Programmers/src/KAKAO2022/p2_주차요금계산.java
@@ -4,11 +4,9 @@ import java.util.*;
 
 public class p2_주차요금계산 {
     public int[] solution(int[] fees, String[] records) {
-        HashMap<String, Integer> parking = new HashMap<>(); // 주차 시간 기록
-        HashMap<String, String> history = new HashMap<>(); // 입차 시간 기록
-        HashMap<String, Integer> totalFee = new HashMap<>();
-
         //TreeMap은 키 자동 오름차순 정렬 저장!!
+        TreeMap<String, Integer> parking = new TreeMap<>(); // 주차 시간 기록
+        HashMap<String, String> history = new HashMap<>(); // 입차 시간 기록
 
         for (String record : records) {
             String[] str = record.split(" ");
@@ -17,12 +15,10 @@ public class p2_주차요금계산 {
             String status = str[2];
 
             if (status.equals("IN")) {
-                if(!parking.containsKey(car)) parking.put(car, 0);
                 parking.put(car, parking.getOrDefault(car, 0));
                 history.put(car, time);
-            }else{
-                String start = history.get(car);
-                int cal = calculate(start, time);
+            } else {
+                int cal = calculate(history.get(car), time);
                 parking.put(car, parking.get(car) + cal); //주차 시간 더하기
                 history.put(car, null);
             }
@@ -35,35 +31,27 @@ public class p2_주차요금계산 {
             }
         }
 
-        for (String car : parking.keySet()) {
-            int time = parking.get(car);
-            int fee = 0;
-            if(time <= fees[0]) fee = fees[1];
-            else{
-                time -= fees[0];
-                if(time % fees[2] != 0) time = time/fees[2] + 1;
-                else time = time/fees[2];
-                fee = time * fees[3] + fees[1];
-            }
-            totalFee.put(car, fee);
-        }
+        int[] answer = new int[parking.keySet().size()];
+        int idx = 0;
 
-        int[] answer = new int[totalFee.keySet().size()];
-        List<String> list = new ArrayList<>(totalFee.keySet());
-        Collections.sort(list);
-        for(int i=0; i<list.size(); i++){
-            answer[i] = totalFee.get(list.get(i));
+        for (int time : parking.values()) {
+            int fee = fees[1];
+            time -= fees[0];
+            if (time > 0) {
+                fee += (time % fees[2] == 0 ? time / fees[2] : time / fees[2] + 1) * fees[3];
+            }
+            answer[idx++] = fee;
         }
 
         return answer;
     }
 
-    public static int calculate(String t1, String t2){
-        String hour1 = t1.substring(0,2);
+    public static int calculate(String t1, String t2) {
+        String hour1 = t1.substring(0, 2);
         String minute1 = t1.substring(3);
         int time1 = (Integer.parseInt(hour1) * 60) + Integer.parseInt(minute1);
 
-        String hour2 = t2.substring(0,2);
+        String hour2 = t2.substring(0, 2);
         String minute2 = t2.substring(3);
         int time2 = (Integer.parseInt(hour2) * 60) + Integer.parseInt(minute2);
         return time2 - time1;
