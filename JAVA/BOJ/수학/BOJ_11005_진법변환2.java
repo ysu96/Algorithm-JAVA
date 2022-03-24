2d83bc7675a34679f5527b5fe7ccd1f36db4c5ac
ysu96 <ysu96@naver.com>
2022-03-24 23:29:10 +0900

    abc

--- JAVA/BOJ/수학/BOJ_11005_진법변환2.java
+++ JAVA/BOJ/수학/BOJ_11005_진법변환2.java
@@ -11,6 +11,7 @@ public class BOJ_11005_진법변환2 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         b = Integer.parseInt(st.nextToken());
