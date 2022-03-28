37860ad7aaf813555678892dfba120943a1243d8
ysu96 <ysu96@naver.com>
2022-03-28 23:34:18 +0900

    삼성SW 기출

---
+++ JAVA/BOJ/삼성SW역량테스트/BOJ_20056_파이어볼.java
@@ -0,0 +1,19 @@
+package JAVA.BOJ.삼성SW역량테스트;
+
+public class BOJ_20056_파이어볼 {
+    public static int N, M, K;
+
+    static class Fireball {
+        int r, c, m, s, d;
+
+        Fireball(int r, int c, int m, int s, int d) {
+            this.r = r;
+            this.c = c;
+            this.m = m;
+            this.s = s;
+            this.d = d;
+        }
+    }
+
+    
+}
