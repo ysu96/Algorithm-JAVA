1c297e51fdac7fd48744298391f4321f0720b509
ysu96 <ysu96@naver.com>
2022-04-01 15:14:03 +0900

    삼성SW 기출

--- JAVA/BOJ/삼성SW역량테스트/BOJ_19237_어른상어.java
+++ JAVA/BOJ/삼성SW역량테스트/BOJ_19237_어른상어.java
@@ -10,13 +10,12 @@ import java.util.StringTokenizer;
 
 public class BOJ_19237_어른상어 {
     public static int N, M, K;
-    public static int sharkCnt;
     public static int answer;
     public static int[] sharkDir;
     public static Smell[][] map;
     public static Queue<Shark> sharks;
     public static int[][][] priority;
-    public static int[] dr = {0,-1, 1, 0, 0};
+    public static int[] dr = {0, -1, 1, 0, 0};
     public static int[] dc = {0, 0, 0, -1, 1};
 
 
@@ -61,8 +60,7 @@ public class BOJ_19237_어른상어 {
                 if (num == 0) {
                     map[i][j] = new Smell(0, 0);
                 } else {
-                    map[i][j] = new Smell(num, 4);
-                    sharkCnt++;
+                    map[i][j] = new Smell(num, K);
                     sharks.add(new Shark(num, i, j));
                 }
             }
@@ -70,19 +68,18 @@ public class BOJ_19237_어른상어 {
 
         // 상어 방향
         st = new StringTokenizer(br.readLine());
-        sharkDir = new int[sharkCnt + 1];
-        for (int k = 1; k <= sharkCnt; k++) {
+        sharkDir = new int[M + 1];
+        for (int k = 1; k <= M; k++) {
             int dir = Integer.parseInt(st.nextToken());
             sharkDir[k] = dir;
         }
 
-
         //각 상어의 방향 우선순위가 상어 당 4줄씩 차례대로 주어진다
         // ex ) priority[1][1][0] --> 1번 상어가 1(위쪽)을 향할때 방향의 우선순위 0번
-        priority = new int[sharkCnt + 1][5][4];
-        for (int i = 1; i <= sharkCnt; i++) {
+        priority = new int[M + 1][5][4];
+        for (int i = 1; i <= M; i++) {
             //위 아래 왼쪽 오른쪽
-            //0, 1, 2, 3
+            //1, 2, 3, 4
             for (int j = 1; j <= 4; j++) {
                 st = new StringTokenizer(br.readLine());
                 for (int k = 0; k < 4; k++) {
@@ -94,9 +91,11 @@ public class BOJ_19237_어른상어 {
         while (true) {
             //1번 상어만 격자에 남았는지
             if (isComplete()) break;
+//            printMap();
+//            System.out.println();
 
             // 1000초가 넘었는데 다른 상어가 격자에 남아있는지
-            if (answer > 1000) {
+            if (answer >= 1000) {
                 answer = -1;
                 break;
             }
@@ -111,11 +110,11 @@ public class BOJ_19237_어른상어 {
         System.out.println(answer);
     }
 
-    public static void reduceSmell(){
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
+    public static void reduceSmell() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
                 map[i][j].time--;
-                if(map[i][j].time <= 0) map[i][j].shark = 0;
+                if (map[i][j].time <= 0) map[i][j].shark = 0;
             }
         }
     }
@@ -124,7 +123,7 @@ public class BOJ_19237_어른상어 {
     // 그런 칸이 없으면 자신의 냄새가 있는 칸의 방향으로 잡는다. 이때 가능한 칸이 여러 개일 수 있는데, 그 경우에는 특정한 우선순위를 따른다.
     public static void move() {
         List<Shark> newSharks = new LinkedList<>();
-        Queue<int[]> newSmells = new LinkedList<>(); // r, c, id, time
+        boolean[][] isShark = new boolean[N][N];
 
         while (!sharks.isEmpty()) {
             Shark shark = sharks.poll();
@@ -136,83 +135,65 @@ public class BOJ_19237_어른상어 {
 
             Queue<Shark> candidate1 = new LinkedList<>();
             Queue<Shark> candidate2 = new LinkedList<>();
-            for(int i=1; i<=4; i++){
+            for (int i = 1; i <= 4; i++) {
                 int nr = r + dr[i];
                 int nc = c + dc[i];
-                if(nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
+                if (nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
 
                 //여기선 id에 방향을 넣어줌
-                if(map[nr][nc].shark == 0) candidate1.add(new Shark(i, nr, nc));
-                else if(map[nr][nc].shark == id) candidate2.add(new Shark(i, nr, nc));
+                if (map[nr][nc].time <= 0) candidate1.add(new Shark(i, nr, nc));
+                else if (map[nr][nc].shark == id) candidate2.add(new Shark(i, nr, nc));
             }
 
             int first = 5;
 
             //빈 칸이 있으면
-            if(candidate1.size() != 0){
+            if (candidate1.size() != 0) {
                 while (!candidate1.isEmpty()) {
                     Shark poll = candidate1.poll();
-                    for(int i=0; i<4; i++){
+                    for (int i = 0; i < 4; i++) {
                         int ndir = priority[id][dir][i];
-                        if(poll.id == ndir){
-                            if(first > i) first = i;
+                        if (poll.id == ndir) {
+                            if (first > i) first = i;
                         }
                     }
                 }
-                //이러고 priority[id][shark.dir][first]의 방향으로 가면 됨??
-                int ndir = priority[id][dir][first];
-                int nr = r + dr[ndir];
-                int nc = c + dc[ndir];
-                newSmells.add(new int[]{nr,nc,id,5});
-//                map[nr][nc].shark = id;
-//                map[nr][nc].time = 5;
-                newSharks.add(new Shark(id, nr, nc));
-                sharkDir[id] = ndir;
-
-            }else{
+            } else {
                 //자신의 냄새가 있는 칸의 방향으로 잡기
                 while (!candidate2.isEmpty()) {
                     Shark poll = candidate2.poll();
-                    for(int i=0; i<4; i++){
+                    for (int i = 0; i < 4; i++) {
                         int ndir = priority[id][dir][i];
-                        if(poll.id == ndir){
-                            if(first > i) first = i;
+                        if (poll.id == ndir) {
+                            if (first > i) first = i;
                         }
                     }
                 }
-
-                int ndir = priority[id][dir][first];
-                int nr = r + dr[ndir];
-                int nc = c + dc[ndir];
-                newSmells.add(new int[]{nr,nc,id,5});
-//                map[nr][nc].shark = id;
-//                map[nr][nc].time = 5;
-                newSharks.add(new Shark(id, nr, nc));
-                sharkDir[id] = ndir;
             }
-        }
+            //이러고 priority[id][shark.dir][first]의 방향으로 가면 됨??
+            int ndir = priority[id][dir][first];
+            int nr = r + dr[ndir];
+            int nc = c + dc[ndir];
 
-        while (!newSmells.isEmpty()) {
-            int[] poll = newSmells.poll();
-            int r = poll[0];
-            int c = poll[1];
-            int id = poll[2];
-            int time = poll[3];
-
-            if(map[r][c].shark == 0){
-                map[r][c].shark = id;
-                map[r][c].time = time;
-            }else if(map[r][c].shark > id){
-                for (Shark s : newSharks) {
-                    if(s.id == map[r][c].shark){
-                        newSharks.remove(s);
-                        break;
+            //옮기려는곳에 이미 상어가 이동해있으면
+            if (isShark[nr][nc]) {
+                for (Shark sk : newSharks) {
+                    if (sk.r == nr && sk.c == nc) {
+                        sk.id = Math.min(sk.id, id);
+                        map[nr][nc].shark = sk.id;
                     }
                 }
-
-                map[r][c].shark = id;
-                map[r][c].time = time;
+            } else {
+                isShark[nr][nc] = true;
+                newSharks.add(new Shark(id, nr, nc));
+                map[nr][nc].shark = id;
+//                map[nr][nc].time = 5;
             }
+            sharkDir[id] = ndir;
+        }
+
+        for (Shark s : newSharks) {
+            map[s.r][s.c].time = K + 1;
         }
 
         sharks.addAll(newSharks);
@@ -223,6 +204,15 @@ public class BOJ_19237_어른상어 {
             return true;
         } else {
             return false;
+        }
+    }
+
+    public static void printMap() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print("(" + map[i][j].shark + ", " + map[i][j].time + ") ");
+            }
+            System.out.println();
         }
     }
 }
