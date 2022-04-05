3f4a02346534bdf3cdb8eabb6251c80a5b482356
ysu96 <ysu96@naver.com>
2022-04-06 00:55:58 +0900

    삼성

---
+++ JAVA/BOJ/삼성SW역량테스트/BOJ_20057_토네이도.java
@@ -0,0 +1,14 @@
+package JAVA.BOJ.삼성SW역량테스트;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_20057_토네이도 {
+    public static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+    }
+}
