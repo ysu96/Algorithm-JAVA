efa0e57f7d94223c9c8726f35132bab484cc3d38
ysu96 <ysu96@naver.com>
2022-04-15 23:53:44 +0900

    line

--- JAVA/coding-test-java/src/LINE2022/LP4.java
+++ JAVA/coding-test-java/src/LINE2022/LP4.java
@@ -3,7 +3,8 @@ package LINE2022;
 public class LP4 {
     // 틀림
     // * 구간의 기둥을 바꾼다고 해도 인접한 2개의 구간의 총합이 달라지지 않는다 *
-
+    // 누적합이 다르면 그 사이 기둥은 무~!조건 옮기게 되어있음!!
+    
     //누적합 해서 idx별 누적합이 다르면 그 기둥을 바꿈 answer++
     //값이 같으면 횟수 안 더하기
 
@@ -23,10 +24,22 @@ public class LP4 {
     public int solution(int[] arr, int[] brr) {
         int answer = 0;
         boolean headStart = true;
+        //원소가 4개인 경우만 생각해버렸네
         if(brr[0] - arr[0] >= arr[1]){
             headStart = false;
         }
 
+        // [8, 1, 1, 8]
+        /// [7, 2, 2, 7]
+        // [2, 5, 1, 5]
+        // [6, 1, 5, 1]
+
+        // [8, 1, 1, 1]
+        // [1. 1, 1, 8]
+
+        // [7, 1, 10, 1,2]
+        // [9, 1, 1, 7 ,3]
+        // [7,9] -> [8,10] -> [18,11] -> [19,18] -> []
         if(headStart){
             for(int i=0; i<arr.length-1; i++){
                 if(arr[i] == brr[i]) continue;
