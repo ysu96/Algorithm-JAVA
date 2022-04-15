97711cb92274f782f730fa87cf8e80e0be177cb3
ysu96 <ysu96@naver.com>
2022-04-15 23:40:10 +0900

    line

--- JAVA/coding-test-java/src/LINE2022/LP2.java
+++ JAVA/coding-test-java/src/LINE2022/LP2.java
@@ -1,46 +1,72 @@
 package LINE2022;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class LP2 {
+    static int[][] needs;
+    static int[] scores;
+    static int answer;
 
-    class Sentence {
-        String s;
-        int score;
+    //배열 길이 15
+    // 원소 길이 10만
+    public int solution(String[] sentences, int n) {
+        needs = new int[sentences.length][27]; //27번째는 시프트키
+        scores = new int[sentences.length];
+        answer = 0;
 
-        Sentence(String s, int score) {
-            this.s = s;
-            this.score = score;
+        // 문장별 점수, 문장 별 필요한 키 구하기
+        for (int i=0; i<sentences.length; i++) {
+            String str = sentences[i];
+            for(int j=0; j<str.length(); j++){
+                char c = str.charAt(j);
+                if('A' <= c && c <= 'Z'){
+                    scores[i]++;
+                    needs[i][26] = 1; // shift key
+                    needs[i][c-'A'] = 1;
+                }
+                else if('a' <= c && c <= 'z'){
+                    needs[i][c-'a'] = 1;
+                }
+                scores[i]++;
+            }
         }
+        int[] need = new int[27];
+        dfs(0, need, 0, sentences.length, n);
+        System.out.println(answer);
+        // s : sentences 길이
+        return answer;
     }
 
-    public int solution(String[] sentences, int n) {
-        int answer = -1;
-        //점수 크기 순으로 정렬? || 길어순으로 정렬?
-        //점수 큰거 순으로 해보면서 점수 세기
-        // 그 다음 점수??
-        // 그리디 같은데
-
-        //배열 길이 15
-        // 원소 길이 10만
-        List<Sentence> list = new ArrayList<>();
-        // 일단 반복문으로 점수 구해놓기?
-        for (String sentence : sentences) {
-            int score = sentence.length();
-            for (int i = 0; i < sentence.length(); i++) {
-                if (sentence.charAt(i) >= 65 && sentence.charAt(i) <= 90) {
-                    score++;
-                }
+    public void dfs(int x, int need[], int score, int s, int n){
+        // 문장 다 돌았으면
+        if (x == s) {
+            int cnt = 0;
+            for (int i = 0; i <= 26; i++) {
+                if (need[i] >= 1) cnt += 1;
             }
-            list.add(new Sentence(sentence, score));
+            // 필요한 문자 수가 n개 이하면
+            if (cnt <= n) {
+                answer = Math.max(answer, score);
+            }
+            return;
         }
 
-        //최대 15번 가장 먼저 해결할 거 찾기??
-        for (int i = 0; i < sentences.length; i++) {
-
+        // x번 문자열을 고르는 경우
+        for (int i = 0; i <= 26; i++) {
+            need[i] += needs[x][i];
         }
+       dfs(x + 1, need, score + scores[x], s, n);
 
-        return answer;
+        // x번 문자열을 고르지 않는 경우
+        for (int i = 0; i <= 26; i++) {
+            need[i] -= needs[x][i];
+        }
+        dfs(x + 1, need, score, s, n);
+    }
+
+    public static void main(String[] args) {
+        LP2 lp2 = new LP2();
+        String[] sen = new String[]{"line in line", "LINE", "in lion"};
+        String[] sen2 = new String[]{"ABcD", "bdbc", "a", "Line neWs"};
+        lp2.solution(sen, 5);
+        lp2.solution(sen2, 7);
     }
 }
