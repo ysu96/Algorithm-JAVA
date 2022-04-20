d4f3bd40313e0e02e92290fc16e0dff71ad342bc
ysu96 <ysu96@naver.com>
2022-04-21 00:05:09 +0900

    bc2

--- JAVA/coding-test-java/src/오늘의집/Bucketplace2_new.java
+++ JAVA/coding-test-java/src/오늘의집/Bucketplace2_new.java
@@ -2,7 +2,33 @@ package 오늘의집;
 
 public class Bucketplace2_new {
     public String solution(String call) {
+        String copied_str = new String(call);
+        call = call.toLowerCase();
+        int n = call.length();
+        int[] count = new int[26];
+        int maxval = 0;
+        for (int i = 0; i < n; i++) {
+            char c = call.charAt(i);
+            count[c - 'a']++;
+            maxval = Math.max(maxval, count[c - 'a']);
+        }
 
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            char c = call.charAt(i);
+            if (maxval != count[c - 'a']) {
+                sb.append(copied_str.charAt(i));
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Bucketplace2_new bc2 = new Bucketplace2_new();
+        System.out.println(bc2.solution("abcabcdefabc"));
+        System.out.println(bc2.solution("abxdeydeabz"));
+        System.out.println(bc2.solution("abcabca"));
+        System.out.println(bc2.solution("ABCabcA"));
     }
 }
